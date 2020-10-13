1485adc199731ae5ef676a8ae9156ef9dcfb7e04
Ano Ndachena <anotidaishendachena@gmail.com>
2020-10-13 14:23:30 -0700

    fix some typos

--- 03_dyn_memory/array_in_heap.c
+++ 03_dyn_memory/array_in_heap.c
@@ -5,15 +5,17 @@
 
 int main(int argc, char const *argv[])
 {
-    int** b = (int **) malloc(sizeof(int *) * 5); // b is a local pointer. It points to an array of pointers in the heap.
-    int** d = b;
+    int** b = (int **) malloc(sizeof(int *) * 2); // b is a local pointer. It points to an array of pointers in the heap.
+    int** d = b; // d is also a double pointer. points to whatever b is pointing to.
 
-    b[0] = (int *) malloc (sizeof(int) * 2);
-    b[1] = (int *) malloc (sizeof(int) * 2); // 2x2 array in the heap
+    // 2 x 2 array in heap
+    b[0] = (int *) malloc (sizeof(int) * 2); 
+    b[1] = (int *) malloc (sizeof(int) * 2);
 
-    b[0][0] = 1; b[0][1] = 2;
-    b[1][0] = 3; b[1][1] = 4;
+    b[0][0] = 1; b[0][1] = 2; // [1, 2]
+    b[1][0] = 3; b[1][1] = 4; // [3, 4]
 
+    // many ways to deference the values
     printf("%d ", (*( d + 0 ))[0] );
     printf("%d\n", *( d[0] + 1 ) );
     printf("%d ", **(d+1));
